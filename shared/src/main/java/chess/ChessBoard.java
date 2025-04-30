7473ab66ff64f0e1b1d1c4b153744b84fc8008ad
Kr1msonReaper <Clifton.JeffersonD@gmail.com>
2025-04-30 12:47:38 -0600

    Filled out the board properly this time, but my equals and hash comparisons aren't working. Gotta fix those.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,10 +10,14 @@ import java.util.List;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    List<ChessPosition> boardPositions;
+    List<ChessPosition> boardPositions = new ArrayList<>();
 
     public ChessBoard() {
-        resetBoard();
+        for(int x = 0; x < 8; x++){
+            for(int y = 0; y < 8; y++){
+                boardPositions.add(new ChessPosition(x, y));
+            }
+        }
     }
 
     @Override
@@ -33,7 +38,7 @@ public class ChessBoard {
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
         for(ChessPosition pos : boardPositions){
-            if(pos == position){
+            if(pos.getRow() == position.getRow() && pos.getColumn() == position.getColumn()){
                 pos.setPiece(piece);
             }
         }
@@ -48,8 +53,17 @@ public class ChessBoard {
      */
     public ChessPiece getPiece(ChessPosition position) {
         for(ChessPosition pos : boardPositions){
-            if(pos == position){
+            if(pos.getRow() == position.getRow() && pos.getColumn() == position.getColumn()){
                 return pos.getPiece();
+            }
+        }
+        return null;
+    }
+
+    public ChessPosition getPosition(int x, int y){
+        for(ChessPosition pos : boardPositions){
+            if(pos.getRow() == x && pos.getColumn() == y){
+                return pos;
             }
         }
         return null;
@@ -60,10 +74,37 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        boardPositions.clear();
+
         for(int x = 0; x < 8; x++){
             for(int y = 0; y < 8; y++){
                 boardPositions.add(new ChessPosition(x, y));
             }
         }
+        // Pawns
+        for(int i = 0; i < 8; i++){
+            getPosition(1, i).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            getPosition(6, i).setPiece(new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+        // Everything else
+        getPosition(0, 0).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        getPosition(0, 1).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        getPosition(0, 2).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        getPosition(0, 3).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        getPosition(0, 4).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        getPosition(0, 5).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        getPosition(0, 6).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        getPosition(0, 7).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        getPosition(7, 0).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        getPosition(7, 1).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        getPosition(7, 2).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        getPosition(7, 3).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        getPosition(7, 4).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        getPosition(7, 5).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        getPosition(7, 6).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        getPosition(7, 7).setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
     }
 }
+
