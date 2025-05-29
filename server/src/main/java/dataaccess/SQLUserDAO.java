89e233cbcab662aa83c6f19c31b6cb39afbf69b2
Kr1msonReaper <Clifton.JeffersonD@gmail.com>
2025-05-28 19:41:41 -0600

    Filled out the sql for userDAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,10 +1,13 @@
 package dataaccess;
 
+import model.GameData;
 import model.UserData;
 import org.eclipse.jetty.server.Authentication;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
+import static server.Server.GSON;
 
 public class SQLUserDAO implements UserDAO {
     public Collection<UserData> users = new ArrayList<>();
@@ -12,7 +15,7 @@ public class SQLUserDAO implements UserDAO {
     public int createUser(UserData data) throws DataAccessException {
         DatabaseManager.createDatabase();
         users.add(data);
-
+        DatabaseManager.executeSQL("INSERT INTO users (userData)", GSON.toJson(data));
         return 200;
     }
 
@@ -25,15 +28,25 @@ public class SQLUserDAO implements UserDAO {
         return false;
     }
 
-    public UserData getUser(String username){
-        for(UserData user : users){
-            if(user.username().equals(username)){
-                return user;
+    public UserData getUser(String username) throws DataAccessException {
+        Collection<String> jsonDumps = new ArrayList<>();
+        Collection<UserData> converted = new ArrayList<>();
+
+        jsonDumps = DatabaseManager.getTableContents("users", "userData");
+
+        for(String dump : jsonDumps){
+            converted.add(GSON.fromJson(dump, UserData.class));
+        }
+
+        for(UserData data : converted){
+            if(data.username().equals(username)){
+                return data;
             }
         }
+
         return null;
     }
-    public void removeAll(){
-        users.clear();
+    public void removeAll() throws DataAccessException {
+        DatabaseManager.executeSQL("DELETE FROM users");
     }
 }
