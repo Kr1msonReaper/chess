12de6c15dd1a75476a677de340f091c0f0f9e506
Kr1msonReaper <Clifton.JeffersonD@gmail.com>
2025-06-02 18:17:52 -0600

    Added several tests.

--- client/src/test/java/client/ServerFacade.java
+++ client/src/test/java/client/ServerFacade.java
@@ -1,36 +1,67 @@
 package client;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Collection;
+
+import service.*;
 
 public class ServerFacade {
     private final int port;
     private final String baseUrl;
     public static final Gson GSON = new Gson();
+    public static ServerFacade facade;
 
     public ServerFacade(int port) {
         this.port = port;
         this.baseUrl = "http://localhost:" + port;
+        facade = this;
     }
 
-    public String sendGetRequest(String endpoint) throws IOException {
+    public String sendGetRequest(String endpoint, String auth) throws IOException {
         URL url = new URL(baseUrl + endpoint);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
+        if(!auth.isEmpty()){
+            connection.setRequestProperty("Authorization", auth);
+        }
 
         return readResponse(connection);
     }
 
-    public String sendPostRequest(String endpoint, String jsonBody) throws IOException {
+    public String sendPostRequest(String endpoint, String jsonBody, String auth) throws IOException {
         URL url = new URL(baseUrl + endpoint);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
+        if(!auth.isEmpty()){
+            connection.setRequestProperty("Authorization", auth);
+        }
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            os.write(jsonBody.getBytes());
+            os.flush();
+        }
+
+        return readResponse(connection);
+    }
+
+    public String sendDeleteRequest(String endpoint, String jsonBody, String auth) throws IOException {
+        URL url = new URL(baseUrl + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("DELETE");
+        connection.setRequestProperty("Content-Type", "application/json");
+        if(!auth.isEmpty()){
+            connection.setRequestProperty("Authorization", auth);
+        }
         connection.setDoOutput(true);
 
         try (OutputStream os = connection.getOutputStream()) {
@@ -56,7 +87,7 @@ public class ServerFacade {
     }
 
     public AuthData register(UserData data) throws IOException {
-        String response = sendPostRequest("/user", GSON.toJson(data));
+        String response = sendPostRequest("/user", GSON.toJson(data), "");
         try {
             AuthData responseAuth = GSON.fromJson(response, AuthData.class);
             return responseAuth;
@@ -64,4 +95,28 @@ public class ServerFacade {
             return null;
         }
     }
+
+    public AuthData login(UserData data) throws IOException {
+        String response = sendPostRequest("/session", GSON.toJson(data), "");
+        try {
+            AuthData responseAuth = GSON.fromJson(response, AuthData.class);
+            return responseAuth;
+        } catch(Exception e){
+            return null;
+        }
+    }
+
+    public void logout(AuthData data) throws IOException {
+        sendDeleteRequest("/session", "", data.authToken());
+    }
+
+    public void createGame(AuthData data, CreateGameRequest req) throws IOException {
+        sendPostRequest("/game", GSON.toJson(req), data.authToken());
+    }
+
+    public Collection<GameData> listGames(AuthData data) throws IOException {
+        ListGamesResult res = GSON.fromJson(sendGetRequest("/game", data.authToken()), ListGamesResult.class);
+        return res.games;
+    }
+
 }
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,14 +1,17 @@
 package client;
 
 import client.ServerFacade;
+import dataaccess.DataAccessException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.AuthData;
 import model.UserData;
 import server.Server;
+import service.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
@@ -29,18 +32,67 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
-    @Test
-    public void sampleTest() {
-        // things to do here:
-        assertTrue(true);
-    }
-
     @Test
     void register() throws Exception {
         UserData reqData = new UserData("player1", "password", "p1@email.com");
         AuthData authData = facade.register(reqData);
         assertTrue(authData.authToken().length() > 10);
+        UserData reqData2 = new UserData("player1", "password", "p1@email.com");
+        AuthData authData2 = facade.register(reqData2);
+        assertNull(authData2.authToken());
+    }
+
+    @Test
+    void login() throws Exception {
+        UserData prereqData = new UserData("player1", "password", "p1@email.com");
+        facade.register(prereqData);
+
+        UserData reqData = new UserData("player1", "password", "p1@email.com");
+        AuthData authData = facade.login(reqData);
+        assertTrue(authData.authToken().length() > 10);
+        UserData reqData2 = new UserData("non-existing-player", "password", "p1@email.com");
+        AuthData authData2 = facade.login(reqData2);
+        assertNull(authData2.authToken());
+    }
+
+    @Test
+    void logout() throws Exception {
+        UserData prereqData = new UserData("player1", "password", "p1@email.com");
+        AuthData data = facade.register(prereqData);
+        facade.logout(data);
+        assertTrue(server.authDAO.getAll().size() == 0);
+        assertFalse(!server.authDAO.getAll().isEmpty());
+    }
+
+    @Test
+    void createGame() throws Exception {
+        UserData prereqData = new UserData("player1", "password", "p1@email.com");
+        AuthData data = facade.register(prereqData);
+        CreateGameRequest newGameReq = new CreateGameRequest();
+        newGameReq.gameName = "New Game";
+        facade.createGame(data, newGameReq);
+
+        assertTrue(server.gameDAO.getGames().size() == 1);
+        assertFalse(server.gameDAO.getGames().isEmpty());
+    }
+
+    @Test
+    void listGames() throws Exception {
+        UserData prereqData = new UserData("player1", "password", "p1@email.com");
+        AuthData data = facade.register(prereqData);
+        CreateGameRequest newGameReq = new CreateGameRequest();
+        newGameReq.gameName = "New Game";
+        facade.createGame(data, newGameReq);
+
+        assertTrue(server.gameDAO.getGames().size() == facade.listGames(data).size());
+        assertFalse(server.gameDAO.getGames().isEmpty());
+    }
+
+    @BeforeEach
+    public void clearDB() throws DataAccessException {
+        server.authDAO.removeAll();
+        server.gameDAO.deleteAll();
+        server.userDAO.removeAll();
     }
 
 }
